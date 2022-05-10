af9a0b08f35a97853e69bef4279ba036fad9749b
Ben Muhlmann <bmuhlmann@mosaicdatascience.com>
2022-05-09 18:41:18 -0700

    update comments to reflect use of anamoly_frac

--- sklearn_implementation.py
+++ sklearn_implementation.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 
 # generate random 2d data
 n = 4000
-train_frac = 0.75
-test_start_index = int(n*train_frac)
 x = np.random.normal(10, 1, n)
 y = np.random.normal(10, 1, n)
 
+# set train fraction and anomaly fraction
+train_frac = 0.75
+test_start_index = int(n*train_frac)
 anomaly_frac = 0.05
 n_anomaly = int((1-train_frac)*anomaly_frac*n)
 
@@ -21,27 +22,21 @@ iforest = IsolationForest()
 iforest.fit(X = data.iloc[0:test_start_index])
 class_probs = iforest.score_samples(data.iloc[test_start_index:])
 
-# get the indexes of normal points and anomalies
-# IsolationForest.predict() outputs an array with 1's and -1's, thus the addition and bool casting
-
+# in sklearn's implementation, scores are negative.
+# the more negative, the more anamolous, so we sort scores ascending
 test_df = data.iloc[3000:].copy()
 test_df['class_probs'] = class_probs
 test_df = test_df.sort_values(by='class_probs')
 
+# scatter 'normal' and anomalous data
 plt.figure()
 plt.ion()
 plt.show()
 plt.scatter(test_df.iloc[0:n_anomaly]['x'], test_df.iloc[0:n_anomaly]['y'], color='orange', label='anomaly')
 plt.scatter(test_df.iloc[n_anomaly:]['x'], test_df.iloc[n_anomaly:]['y'], color='blue', label='normal point')
-
+plt.legend()
 plt.pause(10)
 
-
-# show points classified as anomalies and normal points
-
-# plt.scatter(data.iloc[test_start_index:][index_normals]['x'], data.iloc[test_start_index:][index_normals]['y'], color='blue', label='normal point')
-# plt.scatter(data.iloc[test_start_index:][index_anomalies]['x'], data.iloc[test_start_index:][index_anomalies]['y'], color='orange', label='anomaly')
-# plt.legend()
 
 
 
